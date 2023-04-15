1864a23c18296138df3e594e26e401173c949716
Bacchos <dionysisterzios@gmail.com>
2023-04-15 21:51:13 +0300

    Lint code

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-const bookForm = document.querySelector('#bookForm');
+const bookForm = document.querySelector('#bookForm')
 
-const myLibrary = [];
+const myLibrary = []
 
-function Book(title, author, pages) {
-    this.title = title
-    this.author = author
-    this.pages = pages
+function Book (title, author, pages) {
+  this.title = title
+  this.author = author
+  this.pages = pages
 }
 
-function addBookToLibrary(event) {
-    event.preventDefault();
+function addBookToLibrary (event) {
+  event.preventDefault()
 
-    let title = document.querySelector('input[name="title"]').value;
-    let author = document.querySelector('input[name="author"]').value;
-    let pages = document.querySelector('input[name="pages"]').value;
+  const title = document.querySelector('input[name="title"]').value
+  const author = document.querySelector('input[name="author"]').value
+  const pages = document.querySelector('input[name="pages"]').value
 
-    const book = new Book(title, author, pages);
+  const book = new Book(title, author, pages)
 
-    myLibrary.push(book);
-    displayBook();
+  myLibrary.push(book)
+  displayBook()
 }
 
-function displayBook() {
-    const library = document.querySelector('#library');
+function displayBook () {
+  const library = document.querySelector('#library')
 
-    for (const book of myLibrary) {
-        const div = document.createElement('div');
+  for (const book of myLibrary) {
+    const div = document.createElement('div')
 
-        for (const [key, value] of Object.entries(book)) {
-            div.innerHTML += `<p>${value}</p>`;
-        }
-
-        library.appendChild(div);
+    for (const [key, value] of Object.entries(book)) {
+      div.innerHTML += `<p>${value}</p>`
     }
 
-    myLibrary.length = [];
+    library.appendChild(div)
+  }
+
+  myLibrary.length = []
 }
 
-bookForm.addEventListener('submit', addBookToLibrary);
+bookForm.addEventListener('submit', addBookToLibrary)
